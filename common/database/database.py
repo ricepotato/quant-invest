7c470fae5aa2627322047c544d2f1ae91223af95
Sukjun Sagong <ricepotato40@gmail.com>
2019-05-28 20:36:34 +0900

    add database conf file

--- common/database/database.py
+++ common/database/database.py
@@ -1,13 +1,57 @@
 #-*- coding: utf-8 -*-
+import os
+import sys
+import json
+import logging
 
 from sqlalchemy import create_engine, Column, Integer, String, FLOAT
 from sqlalchemy import Index, UniqueConstraint
 from sqlalchemy.schema import ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
+
+
+cur_path = os.path.dirname(__file__)
+comm_path = os.path.join(cur_path, "..")
+
+sys.path.append(comm_path)
+
+import logger.logcfg
+
+log = logging.getLogger("qi.common.database")
+log.setLevel(logging.DEBUG)
+
+def get_conf(conf_path=None):
+    """ json 설정 파일을 읽어온다.
+    기본 설정파일 경로는 파일경로/database.conf
+    설정 파일 경로를 지정하지 않으면 기본 파일경로에서 읽어온다.
+
+    @param conf_path : str
+    @return : dict
+    """
+    log.debug("reading conf file...")
+    if conf_path is None:
+        conf_path = os.path.join(cur_path, "database.conf")
+    
+    log.debug("conf_path=%s", conf_path)
+    try:
+        with open(conf_path, "r") as f:
+            conf_data = f.read()
+        cfg = json.loads(conf_data)
+    except IOError as e:
+        log.error("get config io error. %s", e)
+        return None
+    except ValueError as e:
+        log.error("get config value error. %s", e)
+        return None
+    return cfg
+ 
+cfg = get_conf()
+
+conn_str = "mysql://{}:{}@{}:{}/{}?charset=utf8".format(cfg["user"], cfg["password"], 
+                                           cfg["host"], cfg["port"], cfg["database"])
 Base = declarative_base()
-engine = create_engine('mysql://qi:password@127.0.0.1:3307/qi?charset=utf8', 
-                        convert_unicode=False) 
+engine = create_engine(conn_str, convert_unicode=False) 
 
 class Company(Base):
     __tablename__ = "company"
