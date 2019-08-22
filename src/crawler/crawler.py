c346d1183e8adc567f78b1dcc2fec4eb38b7bf2c
Sukjun Sagong <ricepotato40@gmail.com>
2019-08-22 19:42:19 +0900

    crawler 기능 구현, test 추가

--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -3,14 +3,8 @@
 import os
 import sys
 import logging
-
-cur_path = os.path.dirname(__file__)
-base_path = os.path.abspath(os.path.join(cur_path, ".."))
-#comm_path = os.path.abspath(os.path.join(cur_path, "..", "common"))
-
-sys.path.append(base_path)
-
-import common.logger.logcfg
+from bs4 import BeautifulSoup as BS
+import requests
 
 log = logging.getLogger("qi.crawler.crawler")
 
@@ -19,7 +13,50 @@ class CompGuideCrawler(object):
     company guide site 에 방문하여 financial report data 를 가져온다.
     """
     def __init__(self):
-        pass
+        self.url = url = "http://comp.fnguide.com/SVO2/asp/SVD_Main.asp?pGB=1&gicode=A{}"
+        self.text = None
+    
+    def _get_text_from_url(self, url):
+        log.debug("getting text from url. url=%s", url)
+        r = requests.get(url)
+        self.text = r.text
+        return r.text
+
+    def _get_url(self, gicode):
+        url = self.url.format(gicode)
+        return url
+    
+    def _get_text_from_selector(self, bs, selector):
+        log.debug("getting text from selector. sel=%s", selector)
+        obj = bs.select(selector)
+        return obj[0].text.strip()
+
+    def _parse_page(self, text):
+        bs = BS(text, "html.parser")        
+        #sel_period = "#highlight_D_A > table > thead > tr.td_gapcolor2 > th:nth-child(1)" <- 첫번째 period
+        #roa = "#highlight_D_A > table > tbody > tr:nth-child(16) > td:nth-child(2)" <- 첫번째 period 의 roa
+        #roe = "#highlight_D_A > table > tbody > tr:nth-child(17) > td:nth-child(2)" <- 첫번째 period 의 roa
+        #per = "#highlight_D_A > table > tbody > tr:nth-child(21) > td:nth-child(2)"
+        #pbr = "#highlight_D_A > table > tbody > tr:nth-child(22) > td:nth-child(2)"
+
+        res = {}
+        for idx in range(1, 4):
+            sel_period = f"#highlight_D_A > table > thead > tr.td_gapcolor2 > th:nth-child({idx})"
+            sel_roa = f"#highlight_D_A > table > tbody > tr:nth-child(16) > td:nth-child({idx+1})"
+            sel_roe = f"#highlight_D_A > table > tbody > tr:nth-child(17) > td:nth-child({idx+1})"
+            sel_per = f"#highlight_D_A > table > tbody > tr:nth-child(21) > td:nth-child({idx+1})"
+            sel_pbr = f"#highlight_D_A > table > tbody > tr:nth-child(22) > td:nth-child({idx+1})"
+            
+            period = self._get_text_from_selector(bs, sel_period)
+            roa = self._get_text_from_selector(bs, sel_roa)
+            roe = self._get_text_from_selector(bs, sel_roe)
+            per = self._get_text_from_selector(bs, sel_per)
+            pbr = self._get_text_from_selector(bs, sel_pbr)
+            period = period.replace("/", "-")
+            res[period] = {"roa":roa, "roe":roe, "per":per, "pbr":pbr}
+            log.debug("getting data. period=%s, data=%s", period, res[period])
+        
+        return res
 
     def get_fr_data(self, comp_code, period):
         """ 종목코드 입력 시 roe, roa, per, pbr 값을 가져와 return 한다.
@@ -28,7 +65,10 @@ class CompGuideCrawler(object):
         @return : dict
         """
 
-        return {"comp_code":comp_code, "period":period, "roe":0, "roa":0, "per":0, "pbr":0}
+        url = self._get_url(comp_code)
+        text = self._get_text_from_url(url)
+        res = self._parse_page(text)
+        return res
 
 def main():
     log.info("hello world")
--- src/tests/test_crawler.py
+++ src/tests/test_crawler.py
@@ -9,8 +9,11 @@ base_path = os.path.abspath(os.path.join(cur_path, ".."))
 
 sys.path.append(base_path)
 
-from crwaler import CompGuideCrawler
+from crawler import CompGuideCrawler
 
+log = logging.getLogger("qi")
+log.addHandler(logging.StreamHandler())
+log.setLevel(logging.DEBUG)
 log = logging.getLogger("qi.tests.crawler")
 
 class TestCrawler(unittest.TestCase):
@@ -21,11 +24,35 @@ class TestCrawler(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_crawler(self):
-        comp_code = "053800"
-        
-        self.crawler.get_fr_data()
+    def _get_text(self, comp_code):
+        bin_path = os.path.join(cur_path, "bin")
+        html_filepath = os.path.join(bin_path, f"{comp_code}.html")
+        with open(html_filepath, "r") as f:
+            text = f.read()
+        return text
 
+    def _test_crawler(self):
+        comp_code = "053800"
+        period = "2018-12"
+        
+        res = self.crawler.get_fr_data(comp_code, period)
+        self.assertEqual(res["comp_code"], comp_code)
+        self.assertEqual(res["period"], period)
+        #self.assertEqual(res["roa"], 9.75)
+
+    def test_parse_page(self):
+        comp_code = "053800"
+        text = self._get_text(comp_code)
+        res = self.crawler._parse_page(text)
+        self.assertEqual(res["2018-12"]["roa"], "9.75")
+        self.assertEqual(res["2017-12"]["per"], "33.03")
+        self.assertEqual(res["2016-12"]["roa"], "7.14")
+
+        # includes N/A
+        comp_code = "043100"
+        text = self._get_text(comp_code)
+        res = self.crawler._parse_page(text)
+        self.assertEqual(res["2018-12"]["per"], "N/A")
 
 if __name__ == "__main__":
     unittest.main()
