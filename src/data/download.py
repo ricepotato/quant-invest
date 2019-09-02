8d38a9f74860d6f7e310cb34b8da20413a6922d7
Sukjun Sagong <ricepotato40@gmail.com>
2019-09-02 12:48:07 +0900

    download 실행 프로그램 구현

--- src/data/download.py
+++ src/data/download.py
@@ -8,17 +8,22 @@ base_path = os.path.abspath(os.path.join(cur_path, ".."))
 sys.path.append(base_path)
 
 from common.logger import LogCfg
-#from common.database.database import Database
+from common.database import Database
 from common.database.dao import *
 from crawler.downloader import CompGuideDownloader
 
 log = logging.getLogger("qi.data.download")
+log.setLevel(logging.DEBUG)
 
 def start_download():
-    comp_dao = CompanyDao()
+    db = Database()
+    comp_dao = CompanyDao(db)
     comp_data_list = comp_dao.select()
     code_list = list(map(lambda comp : comp.code, comp_data_list))
     log.info("code_list len=%d", len(code_list))
+
+    downloader = CompGuideDownloader()
+    downloader.start_download(code_list)  
 
 def main():
     log.info("download main.")
