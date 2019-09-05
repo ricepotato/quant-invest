b7aa192283b3e436e2555b749046120e7eb6fedc
Sukjun Sagong <ricepotato40@gmail.com>
2019-09-05 20:49:56 +0900

    json reader 추가

--- src/crawler/reader.py
+++ src/crawler/reader.py
@@ -1,5 +1,6 @@
 #-*- coding: utf-8 -*-
 import os
+import json
 import logging
 
 log = logging.getLogger("qi.crawler.reader")
@@ -10,8 +11,22 @@ class CompFileReader:
     def __init__(self):
         self.data_path = os.path.join(cur_path, "comp_guide_html")
 
-    def read_text(self, comp_id):
-        txt_path = os.path.join(self.data_path, f"{comp_id}.html")
+    def read_text(self, comp_code):
+        txt_path = os.path.join(self.data_path, f"{comp_code}.html")
         with open(txt_path, "r") as f:
             text = f.read()
         return text
+
+class CompJsonReader:
+    def __init__(self):
+        self.data_path = os.path.join(cur_path, "comp_guide_json")
+    
+    def read_json(self, comp_code):
+        json_path = os.path.join(self.data_path, f"{comp_code}.json")
+        try:
+            with open(json_path, "r") as f:
+                text = f.read()
+        except IOError as e:
+            log.warning("json reader io error. comp_code=%s, %s",comp_code, e)
+            return None
+        return json.loads(text)
