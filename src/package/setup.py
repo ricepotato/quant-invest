8cdc2874e530bfcd72aad204271913d09fa532be
Sukjun Sagong <ricepotato40@gmail.com>
2019-09-06 18:52:14 +0900

    setup url 추가

--- src/package/setup.py
+++ src/package/setup.py
@@ -7,5 +7,6 @@ setup(name="qi",
       version="0.1.6", 
       author="sukjun.sagong",
       author_email="ricepotato40@gmail.com",
+      url="https://github.com/ricepotato/quant-invest",
       packages=setuptools.find_packages()
 )
