e8621b7037573629cc2d22741ecc5df782bc07ea
Sukjun Sagong <ricepotato40@gmail.com>
2019-09-19 20:41:04 +0900

    database base

---
+++ src/main/web/qiweb/app/static/app/scripts/stock.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+	$('#stock_tbl').DataTable( {
+		"paging":   false,
+		"ordering": false,
+        "info":     false,
+        "pagingType": "full_numbers"
+	} );
+} );
