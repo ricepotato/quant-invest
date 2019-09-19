22f342a78826137b1ecfe8ba4611ff24ce7f4e4f
Sukjun Sagong <ricepotato40@gmail.com>
2019-09-19 12:43:14 +0900

    server_main 이름 변경 및 경로 설정

--- src/main/server/main.py
+++ src/main/server/main.py
@@ -4,10 +4,10 @@ import sys
 import logging
 
 cur_path = os.path.dirname(__file__)
-base_path = os.path.join(cur_path, "server")
+base_path = os.path.abspath(os.path.join(cur_path, ".."))
 sys.path.append(base_path)
 
-from server.app import server
+from app import server
 
 log = logging.getLogger("qi")
 log.setLevel(logging.DEBUG)
@@ -15,7 +15,7 @@ log.addHandler(logging.StreamHandler())
 log = logging.getLogger("qi.server.main")
 
 def main():
-    server.run()
+    server.run(port=8091)
 
 if __name__ == "__main__":
     main()
