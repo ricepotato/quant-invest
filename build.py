cc1c5348be4a93e0513fb7e0b1cf78db813d9a05
Sukjun Sagong <ricepotato40@gmail.com>
2019-09-19 19:11:52 +0900

    full build 기능 추가, server build class 로 분리

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ import logging
 
 log = logging.getLogger("qi.build")
 log.addHandler(logging.StreamHandler())
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 
 cur_path = os.path.dirname(__file__)
 main_path = os.path.join(cur_path, "src", "main")
@@ -33,23 +33,17 @@ class WorkingDir:
     def owd(self):
         return self.owd
 
-
-class Builder:
-    def __init__(self):
-        self.cur_path = os.path.abspath(os.path.dirname(__file__))
-        self.main_path = os.path.join(self.cur_path, "src", "main")
+class ServerBuilder:
+    def __init__(self, cur_path):
+        self.full = False
+        self.main_path = os.path.join(cur_path, "src", "main")
         self.bld_path = os.path.join(cur_path, "build")
-        self.srv_bld_path = os.path.join(self.bld_path, "server", "main")
-        self.build_action = {
-            "server":self._build_server
-        }
+        self.bld_base_path = os.path.join(self.bld_path, "server", "base")
+        self.bld_main_path = os.path.join(self.bld_path, "server", "main")
 
-    def build(self, target):
-        log.info("build start target=%s", target)
-        self.target = target
-        return self.build_action.get(target)()
+    def run(self, full):
+        self.full = full
 
-    def _build_server(self):
         log.info("build server...")
         res = self._compress("common")
         res = self._compress("data")
@@ -59,9 +53,16 @@ class Builder:
                       ["common", "data", "server"])
 
         for filepath in mod_gzs:
-            self._move_to(filepath, self.srv_bld_path)
+            self._move_to(filepath, self.bld_main_path)
 
         self._run_build()
+
+    def _move_to(self, filepath, dest_path):
+        filename = os.path.split(filepath)[-1]
+        dest_filepath = os.path.join(dest_path, filename)
+        log.info("gz file move %s -> %s", filepath, dest_filepath)
+        os.rename(filepath, dest_filepath)
+        return dest_filepath
 
     def _compress(self, package):
         with WorkingDir(self.main_path) as wd:
@@ -76,27 +77,48 @@ class Builder:
             log.info("compressed:%s", gz_filepath)
             return gz_filepath
 
-    def _move_to(self, filepath, dest_path):
-        filename = os.path.split(filepath)[-1]
-        dest_filepath = os.path.join(dest_path, filename)
-        log.info("gz file move %s -> %s", filepath, dest_filepath)
-        os.rename(filepath, dest_filepath)
-        return dest_filepath
-
     def _run_build(self):
-        with WorkingDir(self.srv_bld_path) as wd:
+        if self.full:
+            with WorkingDir(self.bld_base_path) as wd:
+                log.info("build image... base")
+                res = subprocess.check_output(["./build.sh"])
+                log.info("build image... complete")
+                log.debug(res)
+
+        with WorkingDir(self.bld_main_path) as wd:
+            log.info("build image... main")
             res = subprocess.check_output(["./build.sh"])
             log.debug(res)
+            log.info("build image... complete")
+
+class Builder:
+    def __init__(self):
+        self.cur_path = os.path.abspath(os.path.dirname(__file__))
+        self.build_action = {
+            "server":ServerBuilder
+        }
+
+    def build(self, target, full):
+        log.info("build start target=%s", target)
+        self.full = full
+        self.target = target
+        builder = self.build_action.get(target)(self.cur_path)
+        return builder.run(self.full)
 
 
 def main():
     log.info("build.py run")
     parser = argparse.ArgumentParser()
     parser.add_argument("--target", default=None, help="specify target.", required=True)
+    parser.add_argument("--full", action="store_true", help="specify run full build", required=False)
+    parser.add_argument("--debug", action="store_true", help="specify run debug mode", required=False)
     args = parser.parse_args()
 
+    if args.debug:
+        log.setLevel(logging.DEBUG)
+
     builder = Builder()
-    res = builder.build(args.target)
+    res = builder.build(args.target, args.full)
     log.info("build complete %s", str(res))
 
 if __name__ == "__main__":
