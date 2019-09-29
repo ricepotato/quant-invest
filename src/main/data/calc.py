51f1fd56a2e580bc8a1d32426f08da3467c7df79
ricepotato <sukjun40@naver.com>
2019-09-30 00:37:48 +0900

    수익률 계산기 기능 구현

---
+++ src/main/data/calc.py
@@ -0,0 +1,76 @@
+#-*- coding: utf-8 -*-
+
+import logging
+
+log = logging.getLogger("qi.data.calc")
+
+class DateCalc:
+    def __init__(self, st_date):
+        self.st_date = st_date
+
+    def add(self, val):
+        """ 날짜 계산기 val 값만큼 더한 월까지의 날짜 문자열을 구함 """
+        date_list = self.st_date.split("-")
+        year = int(date_list[0])
+        month = int(date_list[1])
+
+        month += val
+        while month > 12:
+            year += 1
+            month -= 12
+
+        return "{}-{:02}".format(year, month)
+
+    def __add__(self, other: int):
+        return DateCalc(self.add(other))
+
+    def __repr__(self):
+        return self.st_date
+
+class Calc:
+    def __init__(self):
+        self.price_data = None
+
+    def get_list(self, code: str, st_date: str, 
+                 hold: int, period: int) -> dict:
+
+        st = DateCalc(st_date)
+        total = self._get_item(code, str(st), str(st + period))
+        res = {
+            "code":code,
+            "total":total
+        }
+        er_list = []
+        for idx in range(period - hold + 1):
+            end = st + hold
+            item = self._get_item(code, str(st), str(end))
+            er_list.append(item)
+            st = st + 1
+        res["er_list"] = er_list
+
+        total_er = 0
+        for item in er_list:
+            total_er += item["earning_ratio"]
+        avg_er = float(total_er) / float(len(er_list))
+        res["avg_er"] = avg_er
+
+        return res
+
+    def _get_item(self, code, st_date, end_date):
+        st_price = self.price_data.get_price(code, st_date)
+        end_price = self.price_data.get_price(code, end_date)
+        buy_price = st_price["close"]
+        sell_price = end_price["close"]
+        st_datetime = st_price["date"]
+        end_datetime = end_price["date"]
+        er = self._get_er(buy_price, sell_price)
+
+        return {"st":st_date, "st_datetime":st_datetime,
+                "end_datetime":end_datetime, "end":end_date,
+                "buy_price":buy_price, "sell_price":sell_price,
+                "earning_ratio":er}
+
+    def _get_er(self, buy_price, sell_price):
+        """ 수익률 계산 """
+        earn = sell_price - buy_price
+        return float(earn) / float(buy_price) * 100.0
---
+++ src/tests/data/test_calc.py
@@ -0,0 +1,85 @@
+#-*- coding: utf-8 -*-
+import os
+import sys
+import unittest
+import logging
+
+cur_path = os.path.dirname(__file__)
+approot_path = os.path.abspath(os.path.join(cur_path, "..", "..", "main"))
+sys.path.append(approot_path)
+
+from data.calc import DateCalc
+from common.appctx import AppContext
+
+log = logging.getLogger("qi.tests.calc")
+log.addHandler(logging.StreamHandler())
+log.setLevel(logging.DEBUG)
+log = logging.getLogger("qi.tests.calc")
+
+class CalcTestCase(unittest.TestCase):
+    def setUp(self):
+        ctx_json = os.path.join(approot_path, "ctx.json")
+        appctx = AppContext.from_jsonfile(ctx_json)
+        self.calc = appctx.get_bean("calc")
+
+    def tearDown(self):
+        pass
+
+    def test_date_calc(self):
+        dc = DateCalc("2018-01")
+        date_res = dc.add(12)
+        self.assertEqual(date_res, "2019-01")
+        date_res = dc.add(1)
+        self.assertEqual(date_res, "2018-02")
+        date_res = dc.add(6)
+        self.assertEqual(date_res, "2018-07")
+        date_res = dc.add(24)
+        self.assertEqual(date_res, "2020-01")
+        date_res = dc.add(25)
+        self.assertEqual(date_res, "2020-02")
+
+        date_res = dc + 12
+        self.assertEqual(str(date_res), "2019-01")
+
+    def test_calc_long_period(self):
+        code = "000660"
+        hold = 12
+        st_date = "2017-01"
+        period = 48
+        with self.assertRaises(TypeError) as context:
+            res = self.calc.get_list(code, st_date, hold, period)
+
+    def test_calc(self):
+        code = "000660"
+        hold = 2
+        st_date = "2017-01"
+        period = 5
+        res = self.calc.get_list(code, st_date, hold, period)
+        """res_dest = {
+            "code":code,
+            "avg_er":0,
+            "total":{
+                "st":"2017-01", "end":"2017-06", "buy_price":0, "sell_price":0, "earning_ratio":0
+            },
+            "er_list":[
+                {"st":"2017-01", "end":"2017-03", "buy_price":0, "sell_price":0, "earning_ratio":0},
+                {"st":"2017-02", "end":"2017-04", "buy_price":0, "sell_price":0, "earning_ratio":0},
+                {"st":"2017-03", "end":"2017-05", "buy_price":0, "sell_price":0, "earning_ratio":0},
+                {"st":"2017-04", "end":"2017-06", "buy_price":0, "sell_price":0, "earning_ratio":0}
+            ]
+        }"""
+        self.assertEqual(res["code"], code)
+        self.assertEqual(res["total"]["st"], st_date)
+        self.assertEqual(res["total"]["end"], "2017-06")
+
+        self.assertEqual(res["er_list"][0]["st"], st_date)
+        self.assertEqual(res["er_list"][0]["end"], "2017-03")
+
+    def test_calc_get_er(self):
+        res = self.calc._get_er(100, 110)
+        self.assertEqual(res, 10.0)
+        res = self.calc._get_er(100, 90)
+        self.assertEqual(res, -10.0)
+    
+if __name__ == "__main__":
+    unittest.main()
