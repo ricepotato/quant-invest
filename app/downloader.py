a29203e733e6c6f0f61a26b947beea659c35c188
ricepotato <sj@mobills.co.kr>
2020-05-04 21:57:53 +0900

    downloader 이동

--- app/downloader.py
+++ app/downloader.py
@@ -1,31 +1,31 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import os
 import requests
 import logging
 import multiprocessing
 from multiprocessing import Pool
+from app.database.dao import MongoDao
 
-log = logging.getLogger("qi.crawler.downloader")
+log = logging.getLogger("qi.downloader")
+log.addHandler(logging.StreamHandler())
+log.setLevel(logging.DEBUG)
+
+dao = MongoDao()
+
 
 class CompGuideDownloader:
-
     def __init__(self):
         self.base_url = "http://comp.fnguide.com/SVO2/asp/SVD_Main.asp?pGB=1&gicode=A{}"
-        self.path = self._downlad_path()
+        self.path = "data/comp_guide_html"
         self.processes = 10
-
-    def _downlad_path(self):
-        cur_path = os.path.dirname(__file__)
-        download_path = os.path.join(cur_path, "comp_guide_html")
-        return os.path.abspath(download_path)
 
     def start_download(self, comp_ids):
         log.info("start download...")
-        #multiprocessing.set_start_method("spawn")
+        # multiprocessing.set_start_method("spawn")
         with Pool(self.processes) as p:
             res = p.map(self._get_file_req, comp_ids)
-        #res = list(map(lambda comp_id : self._get_file_req(comp_id), comp_ids))
+        # res = list(map(lambda comp_id : self._get_file_req(comp_id), comp_ids))
 
         return res
 
@@ -34,15 +34,27 @@ class CompGuideDownloader:
         filepath = os.path.join(self.path, f"{comp_id}.html")
         url = self.base_url.format(comp_id)
         return self._download(url, filepath)
-        
+
     def _download(self, url, filepath):
         if os.path.exists(filepath):
             log.debug("file already exist. skip.. %s", filepath)
             return filepath
-        
+
         log.debug("request file... url=%s", url)
         res = requests.get(url)
         with open(filepath, "wb") as f:
             f.write(res.content)
         log.debug("download complete. filepath=%s", filepath)
         return filepath
+
+
+def main():
+    downloader = CompGuideDownloader()
+    stock_list = dao.get_stock_list("KOSDAQ")
+    log.info(len(stock_list))
+    code_map = map(lambda item: item["code"], stock_list)
+    downloader.start_download(code_map)
+
+
+if __name__ == "__main__":
+    main()
