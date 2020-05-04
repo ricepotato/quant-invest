a181f6912181a1a9aeffd1c9be59a3f5d0cba0b1
ricepotato <sj@mobills.co.kr>
2020-05-05 00:30:50 +0900

    mongodb dao set , exist 추가

--- app/database/dao.py
+++ app/database/dao.py
@@ -1,12 +1,10 @@
-
 from app.database.mongo import get_client
 
-class MongoDao(object):
 
+class MongoDao(object):
     def __init__(self):
         self.client = get_client()
         self.coll = self.client.qi.stock
-
 
     def insert(self, obj):
         return self.coll.insert_one(obj)
@@ -19,3 +17,14 @@ class MongoDao(object):
         rs = self.coll.find({"market": market})
         return list(rs)
 
+    def set_object(self, code, object):
+        return self.coll.update_one({"code": code}, {"$set": object})
+
+    def exist(self, code, obj_notation):
+        res = self.coll.find_one(
+            {"$and": [{"code": code}, {obj_notation: {"$exists": True}}]}
+        )
+        if res:
+            return True
+        else:
+            return False
